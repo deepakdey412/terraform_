f9eb0686d57251de64346d9c85a7678b96adbe3d
deepakdey412@gmail.com <deepakdey412@gmail.com>
2026-04-17 17:02:23 +0530

    Update cicd

--- main.tf
+++ main.tf
@@ -1,46 +1,31 @@
-provider "aws" { region = "ap-south-1" }
+provider "aws" {
+  region = var.region
+}
 
-# resource "aws_key_pair" "deployer" {
-#   key_name   = "deployer-key"
-#   public_key = file("mykeypair.pub")
-# }
+# Existing Key Pair
+data "aws_key_pair" "existing_key" {
+  key_name = var.key_name
+}
 
-# default AWS VPC
-resource "aws_default_vpc" "default" {}
+# Existing Security Group
+data "aws_security_group" "existing_sg" {
+  id = var.security_group_id
+}
 
-# resource "aws_security_group" "my_security_group" {
-#   name        = "my-sg"
-#   description = "Security group for Terraform practice"
-#   vpc_id      = aws_default_vpc.default.id
-
-#   ingress {
-#     from_port   = 22            # SSH port
-#     to_port     = 22            # SSH port
-#     protocol    = "tcp"         # TCP protocol
-#     cidr_blocks = ["0.0.0.0/0"] # allow SSH from anywhere
-#   }
-
-#   egress {
-#     from_port   = 0             # all ports
-#     to_port     = 0             # all ports
-#     protocol    = "-1"          # all protocols
-#     cidr_blocks = ["0.0.0.0/0"] # allow all outbound traffic
-#   }
-
-#   tags = {
-#     Name = "ssh-only-sg" # tag name
-#   }
-# }
-
+# EC2 Instance
 resource "aws_instance" "my_instance" {
-  ami           = "ami-05d2d839d4f73aafb" # Ubuntu AMI
-  instance_type = var.ec2_instance_type   # instance type
-  # key_name               = aws_key_pair.deployer.key_name            # SSH key
-  # vpc_security_group_ids = [aws_security_group.my_security_group.id] # security group
+  ami           = "ami-05d2d839d4f73aafb"
+  instance_type = var.ec2_instance_type
+
+  key_name = data.aws_key_pair.existing_key.key_name
+
+  vpc_security_group_ids = [
+    data.aws_security_group.existing_sg.id
+  ]
 
   root_block_device {
     volume_size = var.root_block_device_volume_size
-    volume_type = "gp3" # SSD type
+    volume_type = "gp3"
   }
 
   tags = {
--- variable.tf
+++ variable.tf
@@ -1,3 +1,7 @@
+variable "region" {
+  default = "ap-south-1"
+}
+
 variable "ec2_instance_type" {
   description = "The type of EC2 instance to use"
   type        = string
@@ -8,5 +12,14 @@ variable "root_block_device_volume_size" {
   description = "The size of the root block device in GB"
   type        = number
   default     = 18
+}
 
+variable "key_name" {
+  description = "Existing AWS Key Pair"
+  default     = "deployer-key"
+}
+
+variable "security_group_id" {
+  description = "Existing Security Group ID"
+  default     = "sg-0a9ffbff75e8b7f7e"
 }
